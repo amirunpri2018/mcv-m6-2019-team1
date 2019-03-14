9b0af6b92c2ec542ba026c69ce6337408262cba6
Facundo Ferrin <facundo.ferrin@gmail.com>
2019-03-14 22:54:36 +0100

    Setup for week 3 assignment

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 
-from weeks.week2 import main_task1, main_task2, main_task3, main_task4
+import weeks.week2 as w2
+import weeks.week3 as w3
 
 
 if __name__ == '__main__':
-    # main_task1.main()
-    # main_task2.main()
-    main_task3.main()
-    # main_task4.main()
+    # Week 1
+
+    # Week 2
+    # w2.main_task1.main()
+    # w2.main_task2.main()
+    # w2.main_task3.main()
+    # w2.main_task4.main()
+
+    # Week 3
+    # w3.main_task1.main()
+    # w3.main_task2.main()
+    # w3.main_task3.main()
+    # w3.main_task4.main()
---
+++ src/weeks/week3/main_task1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Standard libraries
+import os
+
+# Related 3rd party libraries
+import cv2 as cv
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+
+# Local libraries
+import src
+
+
+# Some constant for the script
+N = 0.01
+GT = 'no'
+DIM = 3
+EXP_NAME = '{}GT_N{}_DIM{}'.format(GT, N, DIM)
+TASK = 'task2'
+WEEK = 'week2'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
